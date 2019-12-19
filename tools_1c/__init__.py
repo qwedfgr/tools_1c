5a09ebfa75975ff7c907ec150cf5fd9eea5008fa
Qwedfgr <georgiykvelidze@gmail.com>
2019-12-19 21:35:07 +0300

    Fix import

--- tools_1c/__init__.py
+++ tools_1c/__init__.py
@@ -1,4 +1,11 @@
 """Top-level package for tools-1c."""
+from __future__ import absolute_import
+
+from .rac import Cluster
+
+
+class Cluster(Cluster):
+    __doc__ = Cluster.__doc__
 
 __author__ = """George Kvelidze"""
 __email__ = 'georgiykvelidze@gmail.com'
